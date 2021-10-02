7991c49160838bcd8f9abd62b8b55b4eaed1c86e
Amanda Misjuwar <amandabmisjuwar@gmail.com>
2021-10-02 15:58:39 -0400

    Add donor table and update user table

--- backend/typescript/models/donor.model.ts
+++ backend/typescript/models/donor.model.ts
@@ -1,21 +1,24 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { Column, DataType, Model, Table, ForeignKey, BelongsTo } from "sequelize-typescript";
 import { DonorRole } from "../types"
 import User from "./user.model"
 
 @Table({ tableName: "donors" })
 export default class Donor extends Model {
   @ForeignKey(() => User)
-  @Column({ type: DataType.STRING })
-  user_id!: string;
+  @Column({ type: DataType.INTEGER })
+  user_id!: number;
+
+  @BelongsTo(() => User)
+  user!: User;
 
   @Column({ type: DataType.ENUM("LocalBusiness", "IndividualDonor") })
   donor_type!: DonorRole;
 
   @Column({ type: DataType.STRING })
-  facebook_link: string;
+  facebook_link!: string;
 
   @Column({ type: DataType.STRING })
-  instagram_link: string;
+  instagram_link!: string;
 
   @Column({ type: DataType.BOOLEAN })
   recurring_donation!: boolean;
