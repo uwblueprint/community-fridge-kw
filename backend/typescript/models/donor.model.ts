5bc5d677d9084dc5ea6232585da7a6e9557434eb
Amanda Misjuwar <amandabmisjuwar@gmail.com>
2021-10-06 21:41:28 -0400

    Update non-null columns

--- backend/typescript/models/donor.model.ts
+++ backend/typescript/models/donor.model.ts
@@ -1,9 +1,10 @@
-import { Column, DataType, Model, Table, ForeignKey, BelongsTo } from "sequelize-typescript";
+import { Column, DataType, Model, Table, ForeignKey, BelongsTo, AllowNull } from "sequelize-typescript";
 import { DonorRole } from "../types"
 import User from "./user.model"
 
 @Table({ tableName: "donors" })
 export default class Donor extends Model {
+  @AllowNull(false)
   @ForeignKey(() => User)
   @Column({ type: DataType.INTEGER })
   user_id!: number;
@@ -11,18 +12,23 @@ export default class Donor extends Model {
   @BelongsTo(() => User)
   user!: User;
 
+  @AllowNull(false)
   @Column({ type: DataType.ENUM("LocalBusiness", "IndividualDonor") })
   donor_type!: DonorRole;
 
+  @AllowNull(true) 
   @Column({ type: DataType.STRING })
   facebook_link?: string;
 
+  @AllowNull(true) 
   @Column({ type: DataType.STRING })
   instagram_link?: string;
 
+  @AllowNull(true) 
   @Column({ type: DataType.BOOLEAN })
   recurring_donor?: boolean;
 
-  @Column({ type: DataType.STRING })
+  @AllowNull(true) 
+  @Column({ type: DataType.STRING})
   business_name?: string;
 }
