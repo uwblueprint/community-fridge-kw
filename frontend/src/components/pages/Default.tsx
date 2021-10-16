7c100c19d695fcdc5c11f1d06aa0ee2409ce1c78
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-10-16 13:47:53 -0400

    chore: remove entity routes

--- frontend/src/components/pages/Default.tsx
+++ frontend/src/components/pages/Default.tsx
@@ -1,41 +1,9 @@
-import React, { useContext } from "react";
-import { useHistory } from "react-router-dom";
+import React from "react";
 
-import * as Routes from "../../constants/Routes";
-import SampleContext from "../../contexts/SampleContext";
 import Logout from "../auth/Logout";
 import RefreshCredentials from "../auth/RefreshCredentials";
 import ResetPassword from "../auth/ResetPassword";
 
-type ButtonProps = { text: string; path: string };
-
-const Button = ({ text, path }: ButtonProps) => {
-  const history = useHistory();
-  const navigateTo = () => history.push(path);
-  return (
-    <button className="btn btn-primary" onClick={navigateTo} type="button">
-      {text}
-    </button>
-  );
-};
-
-const TeamInfoDisplay = () => {
-  const { teamName, numTerms, members, isActive } = useContext(SampleContext);
-  return (
-    <div>
-      <h2>Team Info</h2>
-      <div>Name: {teamName}</div>
-      <div># terms: {numTerms}</div>
-      <div>
-        Members:{" "}
-        {members.map(
-          (name, i) => ` ${name}${i === members.length - 1 ? "" : ","}`,
-        )}
-      </div>
-      <div>Active: {isActive ? "Yes" : "No"}</div>
-    </div>
-  );
-};
 
 const Default = (): React.ReactElement => {
   return (
@@ -45,16 +13,8 @@ const Default = (): React.ReactElement => {
         <Logout />
         <RefreshCredentials />
         <ResetPassword />
-        <Button text="Create Entity" path={Routes.CREATE_ENTITY_PAGE} />
-        <Button text="Update Entity" path={Routes.UPDATE_ENTITY_PAGE} />
-        <Button text="Display Entities" path={Routes.DISPLAY_ENTITY_PAGE} />
-        <Button text="Edit Team" path={Routes.EDIT_TEAM_PAGE} />
-        <Button text="Hooks Demo" path={Routes.HOOKS_PAGE} />
       </div>
-
       <div style={{ height: "2rem" }} />
-
-      <TeamInfoDisplay />
     </div>
   );
 };
--- frontend/src/theme/index.tsx
+++ frontend/src/theme/index.tsx
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 const customTheme = extendTheme({
   fonts: {
+    header: "Poppins",
     body: `Poppins`,
   },
   config: {
