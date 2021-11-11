a5c2ede21833509d0392644fcf3e2ce95b78a60f
Linna <38363056+linnall@users.noreply.github.com>
2021-11-10 21:48:02 -0500

    feat: Scheduling API Clients (#26)
    * initial commit
    
    * added note to test clients after auth is enabled
    
    ran linter

---
+++ frontend/src/APIClients/SchedulingAPIClient.ts
@@ -0,0 +1,99 @@
+import AUTHENTICATED_USER_KEY from "../constants/AuthConstants";
+import { Schedule, UpdatedSchedulingFields } from "../types/SchedulingTypes";
+import { getLocalStorageObjProperty } from "../utils/LocalStorageUtils";
+import baseAPIClient from "./BaseAPIClient";
+
+// TO DO: verify that api clients once authorization is enabled
+const getSchedules = async (): Promise<Schedule[]> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.get("/scheduling", {
+      headers: { Authorization: bearerToken },
+    });
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+
+const getScheduleById = async (scheduleId: string): Promise<Schedule> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.get(`/scheduling/${scheduleId}`, {
+      headers: { Authorization: bearerToken },
+    });
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
+
+const createSchedule = async (schedule: Schedule): Promise<Schedule> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.post(
+      "/scheduling",
+      {
+        ...schedule,
+      },
+      { headers: { Authorization: bearerToken } },
+    );
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
+
+const updateSchedule = async (
+  scheduleId: string,
+  fields: UpdatedSchedulingFields,
+): Promise<Schedule> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.put(
+      `/scheduling/${scheduleId}`,
+      {
+        ...fields,
+      },
+      { headers: { Authorization: bearerToken } },
+    );
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
+
+const deleteSchedule = async (scheduleId: string): Promise<boolean> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.delete(`/scheduling/${scheduleId}`, {
+      headers: { Authorization: bearerToken },
+    });
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export default {
+  getSchedules,
+  getScheduleById,
+  createSchedule,
+  updateSchedule,
+  deleteSchedule,
+};
