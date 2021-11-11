f92e2bc2acd3afd542591d8e9e80178708293ea5
AmandaMisjuwar <37950626+AmandaMisjuwar@users.noreply.github.com>
2021-11-10 23:11:46 -0500

    feat: Create scheduling get started screen (#29)
    * Create scheduling get started step

--- frontend/src/components/pages/Scheduling/GetStarted.tsx
+++ frontend/src/components/pages/Scheduling/GetStarted.tsx
@@ -1,19 +1,32 @@
-import { Button, Container, Text } from "@chakra-ui/react";
+import { Button, Container, Img, Text } from "@chakra-ui/react";
 import React from "react";
 
+import cfImage from "../../../assets/scheduling_getstarted.svg";
 import { SchedulingStepProps } from "./types";
 
-const GetStarted = ({
-  formValues,
-  setForm,
-  navigation,
-}: SchedulingStepProps) => {
+const GetStarted = ({ navigation }: SchedulingStepProps) => {
   const { next } = navigation;
 
   return (
-    // Insert Get Started page here
-    <Container>
-      <Text textStyle="mobileHeader2">This is Get Started page</Text>
+    <Container pl="42px" pr="42px" pt="73px">
+      <Text marginBottom="30px" textStyle="mobileHeader2">
+        Schedule new donation drop off
+      </Text>
+      <Text marginBottom="30px" textStyle="mobileBody">
+        Submit general information of the food you are planning to donate. Pick
+        the date and time you’re available to come in.
+      </Text>
+      <Text marginBottom="30px" textStyle="mobileBody">
+        Availabilities are based upon ensuring consistant food stock in the
+        fridge for as long as possible. Contact admin for specific time
+        requests.
+      </Text>
+      <Img
+        src={cfImage}
+        alt="Community Fridge"
+        marginBottom="50px"
+        width="100%"
+      />
       <Button onClick={next} variant="navigation">
         Get started
       </Button>
