b5f531bc2bc36c9a21c157dd8c89f779fc0e60a1
jossmurphy <43387820+jossmurphy@users.noreply.github.com>
2021-11-11 20:24:50 -0800

    Refactor code

--- frontend/src/components/common/SchedulingProgressBar.tsx
+++ frontend/src/components/common/SchedulingProgressBar.tsx
@@ -2,27 +2,10 @@ import { Flex } from "@chakra-ui/layout";
 import { Step, Steps } from "chakra-ui-steps";
 import React from "react";
 
-import customTheme from "../../theme";
 import { SchedulingProgessBarProps } from "../pages/Scheduling/types";
 
-const Button = {
-  variants: {
-    navigation: {
-      background: "raddish.100",
-      color: "squash.100",
-      width: "100%",
-      fontSize: "16px",
-    },
-    authNavigation: {
-      backgroundColor: "evergreen.100",
-      color: "squash.100",
-      size: "lg",
-      width: "100%",
-    },
-  },
-};
-
 const steps = [{ label: "1" }, { label: "2" }, { label: "3" }, { label: "4" }];
+
 const SchedulingProgressBar = ({
   activeStep,
 }: SchedulingProgessBarProps): JSX.Element => {
