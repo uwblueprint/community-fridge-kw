25b123861f7d8cb04de7d325fadd2f9df710de18
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-11-16 02:00:24 -0500

    feat: add getDonorByUserId service (#38)
    * feat: add getDonorByUserID service
    
    * fix: get donor service

--- backend/typescript/rest/donorRoutes.ts
+++ backend/typescript/rest/donorRoutes.ts
@@ -2,39 +2,42 @@ import { Router } from "express";
 import donorDtoValidator from "../middlewares/validators/donorValidator";
 import DonorService from "../services/implementations/donorService";
 import IDonorService from "../services/interfaces/donorService";
-import { UserDonorDTO } from "../types";
 import getErrorMessage from "../utilities/errorMessageUtil";
 import { sendResponseByMimeType } from "../utilities/responseUtil";
 
 const donorRouter: Router = Router();
 const donorService: IDonorService = new DonorService();
 
-/* Get all donors */
-donorRouter.get("/", async (req, res) => {
-  const contentType = req.headers["content-type"];
-  try {
-    const donors = await donorService.getDonors();
-    await sendResponseByMimeType<UserDonorDTO>(res, 200, contentType, donors);
-  } catch (error) {
-    await sendResponseByMimeType(res, 500, contentType, [
-      {
-        error: getErrorMessage(error),
-      },
-    ]);
-  }
-});
-
-/* Get donor by ID */
-donorRouter.get("/:id", async (req, res) => {
+/* Get all donors and optionally filter by:
+  - id, through URI (ex. /scheduling/1)
+  - userId, through query param (ex. /scheduling/?userId=1)
+*/
+donorRouter.get("/:id?", async (req, res) => {
   const { id } = req.params;
+  const { userId } = req.query;
+
   const contentType = req.headers["content-type"];
 
-  if (!id) {
+  if (id && userId) {
     await sendResponseByMimeType(res, 400, contentType, [
       {
-        error: "Cannot query by missing id.",
+        error: "Cannot query by both id and userId.",
       },
     ]);
+    return;
+  }
+
+  if (!id && !userId) {
+    try {
+      const donors = await donorService.getDonors();
+      await sendResponseByMimeType(res, 200, contentType, donors);
+    } catch (error) {
+      await sendResponseByMimeType(res, 500, contentType, [
+        {
+          error: error.message,
+        },
+      ]);
+    }
     return;
   }
 
@@ -48,6 +51,21 @@ donorRouter.get("/:id", async (req, res) => {
       } catch (error: unknown) {
         res.status(500).json({ error: getErrorMessage(error) });
       }
+    }
+  }
+
+  if (userId) {
+    if (typeof userId !== "string") {
+      res
+        .status(400)
+        .json({ error: "userId query parameter must be a string" });
+      return;
+    }
+    try {
+      const donor = await donorService.getDonorByUserId(userId);
+      res.status(200).json(donor);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
   }
 });
