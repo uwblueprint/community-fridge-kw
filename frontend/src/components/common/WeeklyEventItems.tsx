81314491105c004b179ca8b3f3d5dd1e07b18d28
Akshay <a42saxen@uwaterloo.ca>
2021-11-26 19:33:48 -0500

    Donor information retrieval done

--- frontend/src/components/common/WeeklyEventItems.tsx
+++ frontend/src/components/common/WeeklyEventItems.tsx
@@ -2,17 +2,17 @@ import {
   Badge,
   Container,
   Text,
-  Tfoot,
-  Th,
-  Thead,
-  Tr,
   useDisclosure,
   VStack,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
 import DonorAPIClient from "../../APIClients/DonorAPIClient";
+<<<<<<< HEAD
 import { colorMap, convertTime } from "../../constants/DaysInWeek";
+=======
+import { colorMap } from "../../constants/DaysInWeek";
+>>>>>>> Donor information retrieval done
 import { DonorResponse } from "../../types/DonorTypes";
 import { Schedule } from "../../types/SchedulingTypes";
 import WeeklyEventItemPopUp from "./WeeklyEventItemPopUp";
@@ -27,6 +27,19 @@ const DefaultWeeklyEventItem = ({
   date,
 }: DefaultWeeklyEventItemProps) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [donor, setDonor] = useState<DonorResponse>();
+
+  useEffect(() => {
+    const getDonor = async () => {
+      const donorResponse = await DonorAPIClient.getDonorById(
+        schedule!.donorId,
+      );
+      console.log(donorResponse);
+      setDonor(donorResponse);
+    };
+
+    getDonor();
+  }, []);
 
   const convertTime = (dateToConvert: string): string => {
     return new Date(dateToConvert).toLocaleTimeString(navigator.language, {
@@ -51,6 +64,7 @@ const DefaultWeeklyEventItem = ({
 
   return (
     <>
+<<<<<<< HEAD
       {
         donor ?
           <>
@@ -92,6 +106,49 @@ const DefaultWeeklyEventItem = ({
             />
           </>
           : null
+=======
+    {
+    donor ?
+      <>
+        <Container
+          color="#FAFCFE"
+          borderWidth="1px"
+          borderRadius="8px"
+          borderColor="#D8DDE0"
+          alignItems="center"
+          centerContent
+          py="24px"
+          px="30px"
+          onClick={onOpen}
+        >
+          <Text textAlign="center" textStyle="desktopBodyBold" mb="8px">
+            {donor?.firstName} {donor?.lastName}
+          </Text>
+          <Text textAlign="center" textStyle="desktopSmall" mb="16px">
+            {convertTime(schedule!.startTime)} - {convertTime(schedule!.endTime)}
+          </Text>
+          <Badge
+            color={`${(colorMap as any)[schedule!.frequency]}.100`}
+            backgroundColor={`${(colorMap as any)[schedule!.frequency]}.200`}
+            textStyle="desktopSmall"
+            py="6px"
+            ph="14px"
+          >
+            {" "}
+            {schedule!.frequency}{" "}
+          </Badge>
+        </Container>
+
+        <WeeklyEventItemPopUp
+          isOpen={isOpen}
+          onClose={onClose}
+          onOpen={onOpen}
+          schedule={schedule}
+          donor={donor as DonorResponse}
+        />
+      </>
+      : null
+>>>>>>> Donor information retrieval done
       }
     </>
   );
