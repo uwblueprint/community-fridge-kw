695236200478bb3ab71f7bcd29256a339bca8cc7
Anne Chung <40839109+annechung@users.noreply.github.com>
2021-11-27 14:43:12 -0500

    feat: Scheduling - Volunteer Information page (#49)

--- frontend/src/components/pages/Scheduling/VolunteerInformation.tsx
+++ frontend/src/components/pages/Scheduling/VolunteerInformation.tsx
@@ -1,7 +1,19 @@
-import { Button, Container, HStack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  HStack,
+  Input,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import RadioSelectGroup from "../../common/RadioSelectGroup";
 import SchedulingProgressBar from "../../common/SchedulingProgressBar";
 import { SchedulingStepProps } from "./types";
 
@@ -13,13 +25,122 @@ const VolunteerInformation = ({
 }: SchedulingStepProps) => {
   const { previous, next } = navigation;
   const history = useHistory();
-  //   const { insert form fields for this page here } = formData;
+  const {
+    startTime,
+    endTime,
+    frequency,
+    volunteerNeeded,
+    volunteerTime,
+    pickupLocation,
+    isPickup,
+    notes,
+  } = formValues;
+
+  const volunteerNeededValues = ["Yes", "No"];
+  const volunteerAssistanceValues = [
+    "Pickup (food rescue)",
+    "Unloading (on site)",
+  ];
+  const volunteerRequiredHelperText =
+    "A volunteer is a community fridge member who will assist with donation dropoffs. Information of the volunteer assigned will be provided.";
+
+  const handleChange = (e: boolean | string, name: string) => {
+    setForm({ target: { name, value: e } });
+  };
+
+  const volunteerNeededRadioValue = () => {
+    if (volunteerNeeded !== null) {
+      return volunteerNeeded ? "Yes" : "No";
+    }
+    return "";
+  }
+
+  const isPickupRadioValue = () => {
+    if (isPickup !== null) {
+      return isPickup ? volunteerAssistanceValues[0] : volunteerAssistanceValues[1];
+    }
+    return "";
+  }
 
   return (
-    // Insert Volunteer Information page here
-    <Container>
+    <Container p="30px">
       <SchedulingProgressBar activeStep={2} totalSteps={4} />
-      <Text textStyle="mobileHeader2">Volunteer Information Page</Text>
+      <Text textStyle="mobileHeader2" mt="2em">
+        Volunteer Information
+      </Text>
+      <Box
+        p="1.5em"
+        bg="squash.100"
+        align="left"
+        m="2em 0 3em"
+        borderWidth="1px"
+        borderColor="#6C6C84"
+        borderRadius="5px"
+      >
+        <Text textStyle="mobileHeader4">Proposed dropoff time</Text>
+        <Text textStyle="mobileBody">{new Date(startTime).toDateString()}</Text>
+        <Text textStyle="mobileBody">
+          {new Date(startTime).toLocaleTimeString()}-
+          {new Date(endTime).toLocaleTimeString()}
+        </Text>
+        <Text textStyle="mobileBody">{frequency}</Text>
+      </Box>
+      <RadioSelectGroup
+        name="volunteer-required"
+        label="Do you require a volunteer?"
+        value={volunteerNeededRadioValue()}
+        values={volunteerNeededValues}
+        isRequired
+        helperText={volunteerRequiredHelperText}
+        onChange={(e: string) => {
+          handleChange(e === "Yes", "volunteerNeeded");
+        }}
+      />
+      {volunteerNeeded &&
+        <>
+        <RadioSelectGroup
+          name="is-pickup"
+          label="What do you require volunteer assistance for?"
+          value={isPickupRadioValue()}
+          values={volunteerAssistanceValues}
+          isRequired
+          onChange={(e: string) => {
+            handleChange(e === volunteerAssistanceValues[0], "isPickup");
+          }}
+        />
+        {isPickup &&
+          <FormControl isRequired m="3em 0">
+            <FormLabel>Pickup location:</FormLabel>
+            <Input
+              value={pickupLocation}
+              onChange={(e) => handleChange(e.target.value, "pickupLocation")}
+              placeholder="Enter location"
+              size="lg"
+            />
+          </FormControl>
+        }
+        <FormControl isRequired m="3em 0">
+          <FormLabel>What is the specific time you require assistance?</FormLabel>
+          <Input
+            value={volunteerTime}
+            onChange={(e) => handleChange(e.target.value, "volunteerTime")}
+            placeholder="Enter time"
+            size="lg"
+          />
+        </FormControl>
+        </>
+      }
+      <FormControl m="3em 0">
+        <FormLabel>Additional notes</FormLabel>
+        <FormHelperText mb="1em">
+          Any notes added will be visible to admin.
+        </FormHelperText>
+        <Textarea
+          value={notes}
+          onChange={(e) => handleChange(e.target.value, "notes")}
+          placeholder="john@shawarmaplus.com"
+        />
+      </FormControl>
       <HStack>
         <Button onClick={previous} variant="navigation">
           Back
