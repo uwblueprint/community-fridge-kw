0629ed6d85d3d3b3ef2ba03dea7de15a9e6872b5
Linna <38363056+linnall@users.noreply.github.com>
2021-12-08 22:54:21 -0500

    fix: add logic for creating schedules for recurring donations (#55)
    * add logic for creating multiple schedules for recurring donations
    
    * remove use of await in while loop
    
    * feat: Scheduling - Volunteer Information page (#49)
    
    * style: update mobile home layout ui (#56)
    
    * style: update colors and layout
    
    * chore: revert
    
    * fix: update landing page copy (#57)
    
    * feat: view donation details popup (#54)
    
    * Add everything
    
    * Test changes
    
    * revert docker
    
    * All checks passed
    
    * address pr comments
    
    * update method of getting next drop off
    
    * WIP creating multiple schedules logic, fixed logic for getting next recurringDonationId
    
    * fix posting incorrect start time, recurringDonationId logic to be fixed
    
    * fix logic for daily donations, extended logic to weekly and monthly donations
    
    * add tests for recurring donations
    
    * frontend: modify width of footer
    
    * modify test for creating recurring donation schedules
    
    * help
    
    * fix merge changes
    
    * fix spacing
    
    * fix weekly calendar file changes
    
    * fix weekly event items I'm so sorry
    
    Co-authored-by: Linna Luo <linnaluo@Linnas-MacBook-Pro.local>
    Co-authored-by: Anne Chung <40839109+annechung@users.noreply.github.com>
    Co-authored-by: Hanlin Cheng <h62cheng@uwaterloo.ca>
    Co-authored-by: Akshay Saxena <41586370+akshay2000saxena@users.noreply.github.com>
    Co-authored-by: Shehryar Suleman <shehryarsuleman@gmail.com>

--- backend/typescript/services/implementations/__tests__/schedulingService.test.ts
+++ backend/typescript/services/implementations/__tests__/schedulingService.test.ts
@@ -12,6 +12,7 @@ import Donor from "../../../models/donor.model";
 import SchedulingService from "../schedulingService";
 
 import testSql from "../../../testUtils/testDb";
+import schedulingRouter from "../../../rest/schedulingRoutes";
 
 type SchedulingKey = keyof typeof Scheduling;
 
@@ -52,8 +53,8 @@ const testSchedules = [
     size: "medium",
     isPickup: false,
     dayPart: "Morning (6am - 11am)",
-    startTime: new Date("2021-09-30T00:00:00.000Z"),
-    endTime: new Date("2021-10-01T00:00:00.000Z"),
+    startTime: new Date("2021-09-01T09:00:00.000Z"),
+    endTime: new Date("2021-09-01T00:10:00.000Z"),
     status: "Pending",
     volunteerNeeded: true,
     volunteerTime: "8:00 AM",
@@ -68,13 +69,12 @@ const testSchedules = [
     volunteerTime: "8:00 AM",
     pickupLocation: "location",
     dayPart: "Morning (6am - 11am)",
-    startTime: new Date("2021-09-30T00:00:00.000Z"),
-    endTime: new Date("2021-10-01T00:00:00.000Z"),
+    startTime: new Date("2021-09-01T09:00:00.000Z"),
+    endTime: new Date("2021-09-01T00:10:00.000Z"),
     status: "Pending",
     volunteerNeeded: false,
-    frequency: "Weekly",
-    recurringDonationId: "1",
-    recurringDonationEndDate: new Date("2022-11-01T00:00:00.000Z"),
+    frequency: "Daily",
+    recurringDonationEndDate: new Date("2021-09-03T00:00:00.000Z"),
     notes: "these are the copied notes",
   },
   {
@@ -87,7 +87,6 @@ const testSchedules = [
     status: "Pending",
     volunteerNeeded: false,
     frequency: "Monthly",
-    recurringDonationId: "2",
     recurringDonationEndDate: new Date("2021-10-01T00:06:00.000Z"),
     notes: "these are the copied notes",
   },
@@ -187,7 +186,7 @@ describe("pg schedulingService", () => {
     const endTime: Date = new Date("October 13, 2014 11:13:00");
     const status: Status = Status.APPROVED;
     const dayPart = DayPart.AFTERNOON;
-    const frequency = Frequency.MONTHLY;
+    const frequency = Frequency.ONE_TIME;
     const schedulingToCreate: CreateSchedulingDTO = {
       ...testSchedules[0],
       status,
@@ -209,6 +208,167 @@ describe("pg schedulingService", () => {
           schedulingToCreate[key as keyof CreateSchedulingDTO],
         );
       });
+    }
+  });
+
+  test("create recurring donation schedules", async () => {
+    const startTime: Date = new Date("September 1, 2021 11:00:00");
+    const endTime: Date = new Date("September 1, 2021 12:00:00");
+    const status: Status = Status.APPROVED;
+    const dayPart = DayPart.AFTERNOON;
+    const frequency = Frequency.DAILY;
+
+    const oneTimeDonationScheduleToCreate: CreateSchedulingDTO = {
+      ...testSchedules[0],
+      status,
+      frequency: Frequency.ONE_TIME,
+      dayPart,
+      startTime,
+      endTime,
+    };
+
+    // CREATE ONE-TIME DONATION
+    const resOneTime = await schedulingService.createScheduling(
+      oneTimeDonationScheduleToCreate,
+    );
+
+    const dailySchedulingToCreate: CreateSchedulingDTO = {
+      ...testSchedules[1],
+      status,
+      frequency,
+      dayPart,
+      startTime,
+      endTime,
+    };
+    const currStartDate: Date = new Date(startTime);
+    const currEndDate: Date = new Date(endTime);
+
+    // CREATE DAILY DONATION
+    const resDaily = await schedulingService.createScheduling(
+      dailySchedulingToCreate,
+    );
+    const dailySchedulingDbRes: SchedulingDTO[] = await schedulingService.getSchedulings();
+    // filter out one-time donation schedule
+    const dailySchedules: SchedulingDTO[] = dailySchedulingDbRes.filter(
+      (item) => {
+        return Number(item.recurringDonationId) === 1;
+      },
+    );
+    // calculate expected number of schedules associated with this daily donation
+    startTime.setHours(0, 0, 0, 0);
+    const dailySchedulingObjectsNum: number =
+      (dailySchedulingToCreate.recurringDonationEndDate!.getTime() -
+        startTime.getTime()) /
+        (1000 * 60 * 60 * 24) +
+      1;
+
+    expect(dailySchedules.length).toEqual(dailySchedulingObjectsNum);
+
+    const dailyDonationGroupId = 1;
+    for (let i = 0; i < dailySchedules.length; i += 1) {
+      const tempStartDate: Date = new Date(currStartDate);
+      const tempEndDate: Date = new Date(currEndDate);
+      // check that start and end dates are correct
+      tempStartDate.setDate(currStartDate.getDate() + i);
+      tempEndDate.setDate(currEndDate.getDate() + i);
+      expect(dailySchedules[i].startTime).toEqual(tempStartDate);
+      expect(dailySchedules[i].endTime).toEqual(tempEndDate);
+      // verify recurring donation id is the same for all schedule objects
+      expect(Number(dailySchedules[i].recurringDonationId)).toEqual(
+        dailyDonationGroupId,
+      );
+    }
+
+    // CREATE WEEKLY DONATION
+    const weeklySchedulingToCreate: CreateSchedulingDTO = {
+      ...dailySchedulingToCreate,
+      frequency: Frequency.WEEKLY,
+      recurringDonationEndDate: new Date("2021-09-29T00:00:00.000Z"),
+      startTime: new Date("September 1, 2021 11:00:00"),
+      endTime: new Date("September 1, 2021 12:00:00"),
+    };
+
+    const resWeekly = await schedulingService.createScheduling(
+      weeklySchedulingToCreate,
+    );
+    const weeklySchedulingDbRes: SchedulingDTO[] = await schedulingService.getSchedulings();
+    // filter for only schedules associated with this weekly donation
+    const weeklySchedules: SchedulingDTO[] = weeklySchedulingDbRes.filter(
+      (item) => {
+        return Number(item.recurringDonationId) === 2;
+      },
+    );
+    // calculate expected number of schedules associated with this weekly donation
+    const weeklySchedulingObjectsNum: number =
+      Math.floor(
+        (weeklySchedulingToCreate.recurringDonationEndDate!.getTime() -
+          startTime.getTime()) /
+          (7 * 1000 * 60 * 60 * 24),
+      ) + 1;
+
+    expect(weeklySchedules.length).toEqual(weeklySchedulingObjectsNum);
+
+    const weeklyDonationGroupId = 2;
+    for (let i = 0; i < weeklySchedules.length; i += 1) {
+      const tempStartDate: Date = new Date(currStartDate);
+      const tempEndDate: Date = new Date(currEndDate);
+      // check that start and end dates are correct
+      tempStartDate.setDate(currStartDate.getDate() + i * 7);
+      tempEndDate.setDate(currEndDate.getDate() + i * 7);
+      expect(weeklySchedules[i].startTime).toEqual(tempStartDate);
+      expect(weeklySchedules[i].endTime).toEqual(tempEndDate);
+      // verify recurring donation id is the same for all scheduling objects
+      expect(Number(weeklySchedules[i].recurringDonationId)).toEqual(
+        weeklyDonationGroupId,
+      );
+    }
+
+    // CREATE MONTHLY DONATION
+    const monthlySchedulingToCreate: CreateSchedulingDTO = {
+      ...dailySchedulingToCreate,
+      frequency: Frequency.MONTHLY,
+      recurringDonationEndDate: new Date("2021-02-28T00:00:00.000Z"),
+      startTime: new Date("September 1, 2021 11:00:00"),
+      endTime: new Date("September 1, 2021 12:00:00"),
+    };
+
+    const resMonthly = await schedulingService.createScheduling(
+      monthlySchedulingToCreate,
+    );
+    const resMonthlySchedulingDbRes: SchedulingDTO[] = await schedulingService.getSchedulings();
+    // filter for only scheduling objects associated with this monthly donation
+    const monthlySchedules: SchedulingDTO[] = resMonthlySchedulingDbRes.filter(
+      (item) => {
+        return Number(item.recurringDonationId) === 3;
+      },
+    );
+    // calculate expected number of donations associated with this monthly donation
+    let monthlySchedulingObjectsNum = 0;
+    const dateIncrementor: Date = new Date(currStartDate);
+    dateIncrementor.setHours(0, 0, 0, 0);
+    while (
+      dateIncrementor.valueOf() <=
+      weeklySchedulingToCreate.recurringDonationEndDate!.valueOf()
+    ) {
+      monthlySchedulingObjectsNum += 1;
+      dateIncrementor.setMonth(dateIncrementor.getMonth() + 1);
+    }
+
+    expect(monthlySchedules.length).toEqual(monthlySchedulingObjectsNum);
+
+    const monthlyDonationGroupId = 3;
+    for (let i = 0; i < monthlySchedules.length; i += 1) {
+      const tempStartDate: Date = new Date(currStartDate);
+      const tempEndDate: Date = new Date(currEndDate);
+      // check that start and end dates are correct
+      tempStartDate.setMonth(currStartDate.getMonth() + i);
+      tempEndDate.setMonth(currEndDate.getMonth() + i);
+      expect(monthlySchedules[i].startTime).toEqual(tempStartDate);
+      expect(monthlySchedules[i].endTime).toEqual(tempEndDate);
+      // verify recurring donation id is the same for all scheduling objects
+      expect(Number(monthlySchedules[i].recurringDonationId)).toEqual(
+        monthlyDonationGroupId,
+      );
     }
   });
 
