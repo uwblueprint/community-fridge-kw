d95ed050375478b75dddfc55019587ea942f6f56
Hanlin Cheng <h62cheng@uwaterloo.ca>
2022-01-04 23:18:01 -0500

    style: center text

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -1,11 +1,8 @@
+import { Center } from "@chakra-ui/react";
 import React from "react";
 
 const NotFound = (): React.ReactElement => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>404 Not Found 🙁</h1>
-    </div>
-  );
+  return <Center>404 Not Found 🙁</Center>;
 };
 
 export default NotFound;
