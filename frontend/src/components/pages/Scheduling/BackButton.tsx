e00f88ca30a8700f9b3a55846e5a2c1ea53faffe
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2022-01-19 14:49:18 -0500

    add button files

---
+++ frontend/src/components/pages/Scheduling/BackButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Box, Button } from "@chakra-ui/react";
+import { BackButtonProps } from "./types";
+
+export default function BackButton({isBeingEdited, onSaveClick, previous}: BackButtonProps) {
+    return (
+      <Box mt={10}>
+        {isBeingEdited ? (
+          <Button
+            onClick={onSaveClick}
+            variant="navigation"
+          >
+          Save Changes
+        </Button>
+        ) : (
+        <Button
+          onClick={previous}
+          paddingLeft="0 !important"
+          backgroundColor="transparent"
+        >
+          <ArrowBackIcon w={8} h={5}/> Back
+        </Button>
+        )}
+      </Box>
+    )
+}
