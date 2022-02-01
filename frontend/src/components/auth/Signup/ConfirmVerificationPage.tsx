b711d9bda44fe57e56fa555543624092dec17069
Jessie Peng <jessie.peng2010@gmail.com>
2022-02-01 00:13:16 -0500

    fix: homepage and verification auth icons (#118)
    * add desktop icons
    
    * added verification images
    
    * fix close icon position
    
    * added email verification expiry
    
    * lint
    
    * revert app.tsx changes
    
    * add dashboardVariant
    
    * Update dashboard variant
    
    Co-authored-by: Hanlin Cheng <h62cheng@uwaterloo.ca>
    
    Co-authored-by: Hanlin Cheng <h62cheng@uwaterloo.ca>

--- frontend/src/components/auth/Signup/ConfirmVerificationPage.tsx
+++ frontend/src/components/auth/Signup/ConfirmVerificationPage.tsx
@@ -1,15 +1,16 @@
 import { CloseIcon } from "@chakra-ui/icons";
-import { Button, Container, IconButton, Text } from "@chakra-ui/react";
+import { Button, Container, IconButton, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import confirmVerificationImage from "../../../assets/authentication_complete.svg";
 import * as Routes from "../../../constants/Routes";
 
 const ConfirmVerificationPage = () => {
   const history = useHistory();
 
   return (
-    <Container centerContent pl="42px" pr="42px" pt="31px">
+    <Container variant="dashboardVariant">
       <IconButton
         display={{ md: "none" }}
         aria-label="close page"
@@ -19,25 +20,33 @@ const ConfirmVerificationPage = () => {
       >
         <CloseIcon color="black.100" />
       </IconButton>
-      <Text mt="1rem" textAlign="center" textStyle="mobileHeader1">
-        Thank you for verifying your email address!
-      </Text>
-      <Text
-        mt="1rem"
-        textAlign="center"
-        textStyle="mobileBody"
-        color="hubbard.100"
-      >
-        Almost done! Click &ldquo;Finish&rdquo; to contine.
-      </Text>
-      <Button
-        mt="2rem"
-        width="100%"
-        variant="navigation"
-        onClick={() => history.push(Routes.DASHBOARD_PAGE)}
-      >
-        Finish
-      </Button>
+      <Container centerContent>
+        <Image
+          width="80%"
+          height="80%"
+          src={confirmVerificationImage}
+          alt="Verification email image"
+        />
+        <Text mt="1rem" textAlign="center" textStyle="mobileHeader1">
+          Thank you for verifying your email address!
+        </Text>
+        <Text
+          mt="1rem"
+          textAlign="center"
+          textStyle="mobileBody"
+          color="hubbard.100"
+        >
+          Almost done! Click &ldquo;Finish&rdquo; to contine.
+        </Text>
+        <Button
+          mt="2rem"
+          width="100%"
+          variant="navigation"
+          onClick={() => history.push(Routes.DASHBOARD_PAGE)}
+        >
+          Finish
+        </Button>
+      </Container>
     </Container>
   );
 };
