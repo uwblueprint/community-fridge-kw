91239385360d018cbfd655be38f1b3a979f768ec
Jessie Peng <jessie.peng2010@gmail.com>
2022-02-16 23:43:26 -0500

    lint frontend

--- frontend/src/components/auth/Action.tsx
+++ frontend/src/components/auth/Action.tsx
@@ -31,7 +31,6 @@ const Action = () => {
     }
   };
 
-<<<<<<< HEAD
   const confirmPasswordReset = async () => {
     const confirmPasswordResetResponse = await AuthAPIClient.verifyPasswordResetCode(
       oobCode ?? "",
@@ -50,10 +49,6 @@ const Action = () => {
         <Spinner />
       </Center>
     );
-=======
-  if (emailVerified === EmailVerificationResponse.SUCCESS) {
-    return <ConfirmVerificationPage />;
->>>>>>> 136c326 (fix lint issues)
   }
   if (mode === ActionModes.PASSWORD_RESET) {
     confirmPasswordReset();
