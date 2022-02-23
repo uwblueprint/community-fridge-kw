b44c42ebd97704a536e37dc4aa83289e81ea1bdd
Hanlin Cheng <h62cheng@uwaterloo.ca>
2022-02-22 20:59:34 -0500

    fix: remove 2 week block on scheduling calendar

--- frontend/src/components/pages/Scheduling/SelectDateTime.tsx
+++ frontend/src/components/pages/Scheduling/SelectDateTime.tsx
@@ -338,12 +338,6 @@ const SelectDateTime = ({
     }
   };
 
-  const today = new Date();
-  const getMaxDate = () => {
-    const diff = today.getDate() + 13;
-    return new Date(today.setDate(diff));
-  };
-
   const discardChanges = async () => {
     const scheduleResponse = await SchedulingAPIClient.getScheduleById(id);
 
@@ -383,7 +377,6 @@ const SelectDateTime = ({
         <Calendar
           className={isDesktop ? "rmdp-mobile desktop" : "rmdp-mobile"}
           minDate={new Date()}
-          maxDate={getMaxDate()}
           value={date}
           onChange={handleDateSelect}
         />
