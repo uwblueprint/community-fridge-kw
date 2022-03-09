d096df59d4f09eeea7b56f6a4f9eb349cd6ff3bd
Jessie Peng <jessie.peng2010@gmail.com>
2022-03-08 22:30:14 -0500

    add: checkin types and checkin api client - get, delete

---
+++ frontend/src/APIClients/CheckInAPIClient.ts
@@ -0,0 +1,67 @@
+import { BEARER_TOKEN } from "../constants/AuthConstants";
+import { CheckIn} from "../types/CheckInTypes";
+import baseAPIClient from "./BaseAPIClient";
+
+const getAllCheckIns =  async (): Promise<CheckIn[]> => {
+    try {
+        const { data } = await baseAPIClient.get("/checkin", {
+            headers: { Authorization: BEARER_TOKEN },
+        });
+        return data;
+    } catch (error) {
+        return [];
+    }
+};
+
+const getCheckInById = async (checkInId: string): Promise<CheckIn> => {
+    try {
+        const { data } = await baseAPIClient.get(`/checkin/${checkInId}`, {
+          headers: { Authorization: BEARER_TOKEN },
+        });
+        return data;
+      } catch (error) {
+        return error as CheckIn;
+      }
+};
+
+const getCheckInsByVolunteerId = async (volunteerId: string): Promise<CheckIn[]> => {
+    try {
+        const url = `/checkin/volunteers?volunteerId=${volunteerId}`;
+        const { data } = await baseAPIClient.get(url, {
+          headers: { Authorization: BEARER_TOKEN },
+        });
+        return data;
+      } catch (error) {
+        return error as CheckIn[];
+      }
+};
+
+const deleteCheckIn = async (checkInId: string): Promise<boolean> => {
+    try {
+      await baseAPIClient.delete(`/checkin/${checkInId}`, {
+        headers: { Authorization: BEARER_TOKEN },
+      });
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+const deleteCheckInsByDateRange = async (startDate: string, endDate: string): Promise<boolean> => {
+    try {
+        await baseAPIClient.delete(`/checkin?startDate=${startDate}&endDate=${endDate}`, {
+          headers: { Authorization: BEARER_TOKEN },
+        });
+        return true;
+      } catch (error) {
+        return false;
+      }
+}
+
+export default {
+    getAllCheckIns,
+    getCheckInById,
+    getCheckInsByVolunteerId,
+    deleteCheckIn,
+    deleteCheckInsByDateRange,
+}
