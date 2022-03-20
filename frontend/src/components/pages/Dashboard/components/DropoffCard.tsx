e0b62e8e6a1422ec089c7688dfc4cb28c46adb18
Hanlin Cheng <h62cheng@uwaterloo.ca>
2022-03-20 00:31:27 -0400

    feat: donor dashboard card refresh

--- frontend/src/components/pages/Dashboard/components/DropoffCard.tsx
+++ frontend/src/components/pages/Dashboard/components/DropoffCard.tsx
@@ -1,13 +1,45 @@
-import { CheckIcon, CloseIcon, RepeatIcon } from "@chakra-ui/icons";
-import { Box, HStack, Stack, Text } from "@chakra-ui/react";
-import { format, isToday, isTomorrow } from "date-fns";
+import { Box, Stack, Text, VStack } from "@chakra-ui/react";
+import { format, parse } from "date-fns";
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import VolunteerAPIClient from "../../../../APIClients/VolunteerAPIClient";
 import { getFrequencyColor } from "../../../../constants/DaysInWeek";
 import * as Routes from "../../../../constants/Routes";
 import { Schedule } from "../../../../types/SchedulingTypes";
+import {
+  dateHeadingText,
+  getAssistanceType,
+} from "../../../../utils/DashboardUtils";
 import { DonationFrequency } from "../../Scheduling/types";
+
+const DropoffCardSubInformation = ({
+  description,
+  value,
+  isFrequencyBlock,
+  frequency,
+  frequencyColorScheme,
+}: {
+  description: string;
+  value: string;
+  isFrequencyBlock?: boolean;
+  frequency?: string;
+  frequencyColorScheme?: string;
+}) => (
+  <VStack spacing="4px" alignItems="left" minWidth="10vw">
+    <Text textTransform="uppercase" color="hubbard.100" textStyle="mobileSmall">
+      {description}
+    </Text>
+    <Text color="black.100" textStyle="desktopBody">
+      {isFrequencyBlock && (
+        <Box as="span" textStyle="mobileBodyBold" color={frequencyColorScheme}>
+          {frequency === DonationFrequency.ONE_TIME ? "One time" : frequency}
+        </Box>
+      )}
+      {value}
+    </Text>
+  </VStack>
+);
 
 const DropoffCard = ({ schedule }: { schedule: Schedule }): JSX.Element => {
   const {
@@ -16,92 +48,93 @@ const DropoffCard = ({ schedule }: { schedule: Schedule }): JSX.Element => {
     id,
     frequency,
     volunteerNeeded,
-    recurringDonationEndDate,
+    isPickup,
+    volunteerTime,
+    volunteerId,
   } = schedule;
+  const history = useHistory();
   const frequencyColorScheme = getFrequencyColor(frequency);
+  const [volunteerAssigned, setVolunteerAssigned] = React.useState("");
   const startDateLocal = new Date(startTime);
   const startTimeLocal = format(new Date(startTime), "h:mm aa");
   const endTimeLocal = format(new Date(endTime), "h:mm aa");
-  const formattedRecurringEndDate = format(
-    new Date(recurringDonationEndDate),
-    "MMM d, yyyy",
-  );
 
-  const dateHeadingText = (startDate: Date) => {
-    if (isToday(startDate)) {
-      return "Today";
-    }
-    if (isTomorrow(startDate)) {
-      return "Tomorrow";
-    }
-    return format(startDate, "MMM d, yyyy");
-  };
-
-  const history = useHistory();
+  React.useEffect(() => {
+    const getVolunteerName = async (vId: string) => {
+      if (volunteerId) {
+        const response = await VolunteerAPIClient.getVolunteerById(vId);
+        setVolunteerAssigned(`${response.firstName} ${response.lastName}`);
+      }
+    };
+    getVolunteerName(String(volunteerId));
+  }, []);
 
   return (
     <Box
+      pb="50px"
+      pt="32px"
+      pl="30px"
       mb="24px"
-      mr={{ base: "0px", md: "24px" }}
       borderLeft={`7px solid ${getFrequencyColor(frequency)}`}
       boxShadow="2px 2px 12px rgba(0, 0, 0, 0.08)"
       width={{ base: "default", md: "100%" }}
-      onClick={() => history.push(`${Routes.DASHBOARD_PAGE}/${id}`)}
       overflow="hidden"
     >
-      <Stack
-        direction={["column", "row"]}
-        p="6"
-        display={["default", "flex"]}
-        spacing={["0", "4"]}
-        alignItems="center"
-      >
-        <Text
-          minWidth="125px"
-          textTransform="uppercase"
-          textStyle="mobileSmall"
-          color="hubbard.100"
-          pb={["6px", "0px"]}
+      <VStack alignItems="left">
+        <Stack
+          direction={["column", "row"]}
+          spacing={["4px", "24px"]}
+          mb={["4px", "21px"]}
         >
-          {dateHeadingText(startDateLocal)}
-        </Text>
-        <Text
-          textStyle="mobileHeader4"
-          whiteSpace="nowrap"
-          minWidth="225px"
-          pb={["18px", "0px"]}
-        >
-          {`${startTimeLocal}-${endTimeLocal}`}
-        </Text>
-        {volunteerNeeded ? (
-          <HStack minWidth="225px" pb={["12px", "0px"]}>
-            <CheckIcon color={frequencyColorScheme} />
-            <Text textStyle="mobileBody">Volunteers requested</Text>
-          </HStack>
-        ) : (
-          <HStack minWidth="225px" pb={["12px", "0px"]}>
-            <CloseIcon w={3} color={frequencyColorScheme} mr="4px" />
-            <Text textStyle="mobileBody">No volunteers needed</Text>
-          </HStack>
-        )}
-        <HStack>
-          <RepeatIcon color={frequencyColorScheme} />
-          <Text>
-            <Box
-              as="span"
-              textStyle="mobileBodyBold"
-              color={frequencyColorScheme}
-            >
-              {frequency === DonationFrequency.ONE_TIME
-                ? "One time"
-                : frequency}
-            </Box>
-            {frequency === DonationFrequency.ONE_TIME
-              ? ` Donation`
-              : ` Donation ending on ${formattedRecurringEndDate}`}
+          <Text
+            textStyle="mobileHeader4"
+            whiteSpace="nowrap"
+            pb={["18px", "0px"]}
+          >
+            {dateHeadingText(startDateLocal)}
           </Text>
-        </HStack>
-      </Stack>
+          <Text
+            color="h20.100"
+            textDecoration="underline"
+            onClick={() => history.push(`${Routes.DASHBOARD_PAGE}/${id}`)}
+          >
+            View Details
+          </Text>
+        </Stack>
+        <Stack direction={["column", "row"]} spacing={["20px", "40px"]}>
+          <DropoffCardSubInformation
+            description="Donation Time"
+            value={`${startTimeLocal}-${endTimeLocal}`}
+          />
+          <DropoffCardSubInformation
+            description="Frequency"
+            value={
+              frequency === DonationFrequency.ONE_TIME
+                ? ` Donation`
+                : ` Recurring Donation`
+            }
+            isFrequencyBlock
+            frequency={frequency}
+            frequencyColorScheme={frequencyColorScheme}
+          />
+          <DropoffCardSubInformation
+            description="Volunteer Request Time"
+            value={
+              volunteerTime
+                ? format(parse(volunteerTime, "kk:mm", new Date()), "h:mm aa")
+                : "-"
+            }
+          />
+          <DropoffCardSubInformation
+            description="Assistance Type"
+            value={volunteerNeeded ? getAssistanceType(isPickup!) : "-"}
+          />
+          <DropoffCardSubInformation
+            description="Volunteer Assigned"
+            value={volunteerId ? volunteerAssigned : "-"}
+          />
+        </Stack>
+      </VStack>
     </Box>
   );
 };
