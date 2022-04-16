7cd7e3967110b3a729c380967d5f03e49a82b24b
Shehryar Suleman <shehryarsuleman@gmail.com>
2022-04-16 04:09:31 -0400

    confirm details refactoring

--- frontend/src/components/pages/Scheduling/ConfirmDetails.tsx
+++ frontend/src/components/pages/Scheduling/ConfirmDetails.tsx
@@ -114,10 +114,7 @@ const ConfirmDetails = ({
   };
 
   const getVolunteerData = async () => {
-    if (currentSchedule.volunteerId === null) {
-      return;
-    }
-    if (currentSchedule.volunteerId !== undefined) {
+    if (currentSchedule.volunteerId) {
       const volunteerResponse = await VolunteerAPIClient.getVolunteerById(
         currentSchedule.volunteerId.toString(),
       );
@@ -327,28 +324,25 @@ const ConfirmDetails = ({
                 Assigned volunteer
               </Text>
               <Text textStyle="mobileBody">
-                {currentSchedule.volunteerId === undefined ||
-                currentSchedule.volunteerId === null
-                  ? "-"
-                  : `${currentVolunteer.firstName} ${currentVolunteer.lastName}`}
+                {currentSchedule.volunteerId
+                  ? `${currentVolunteer.firstName} ${currentVolunteer.lastName}`
+                  : "-"}
               </Text>
               <Text textStyle="mobileSmall" color="hubbard.100" pt="1.4em">
                 Email
               </Text>
               <Text textStyle="mobileBody">
-                {currentSchedule.volunteerId === undefined ||
-                currentSchedule.volunteerId === null
-                  ? "-"
-                  : `${currentVolunteer.email}`}
+                {currentSchedule.volunteerId
+                  ? `${currentVolunteer.email}`
+                  : "-"}
               </Text>
               <Text textStyle="mobileSmall" color="hubbard.100" pt="1.4em">
                 Phone
               </Text>
               <Text textStyle="mobileBody">
-                {currentSchedule.volunteerId === undefined ||
-                currentSchedule.volunteerId === null
-                  ? "-"
-                  : `${currentVolunteer.phoneNumber}`}
+                {currentSchedule.volunteerId
+                  ? `${currentVolunteer.phoneNumber}`
+                  : "-"}
               </Text>
             </>
           )}
